0d2e75a630a1a9aceab6c8a5a72e84e4e015b3e7
OrbinWaffle <joshlaiguy@gmail.com>
2023-03-10 13:10:37 -0800

    menu stuff

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader manager;
+
+    [SerializeField] GameObject player;
+
+    private void Awake()
+    {
+        manager = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwitchScene(int sceneIndex)
+    {
+        DontDestroyOnLoad(player);
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void ReloadScene()
+    {
+        player.transform.position = GameObject.FindGameObjectWithTag("SpawnPoint").transform.position;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
