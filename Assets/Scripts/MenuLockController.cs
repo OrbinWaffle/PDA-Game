af0bae118f3f9ad8e64c811a13890d2d2abd61e3
Alex G <alxgomez123@gmail.com>
2023-04-12 19:45:29 -0700

    Added new main menu scene and menu lock script

---
+++ Assets/Scripts/MenuLockController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLockController : MonoBehaviour
+{
+    [SerializeField] int sceneIdLoaded;
+    [SerializeField] float loadWait;
+    [SerializeField] Animator doorAnim;
+
+    IEnumerator OnTriggerEnter(Collider other) 
+    {
+        if (other.CompareTag("Key")) 
+        {
+            doorAnim.enabled = true;
+            yield return new WaitForSeconds(loadWait);
+            SceneLoader.manager.SwitchScene(sceneIdLoaded);
+        }
+    }
+}
